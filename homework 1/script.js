b97755bd25ed2a9c7e5186b7900907a82fa4d8c9
Alexey Lobzov <aleks.lobzov@gmail.com>
2017-10-07 22:33:58 +0300

    second task is done

--- homework 1/script.js
+++ homework 1/script.js
@@ -1,12 +1,51 @@
 function parseAmount(str) {
-    if (typeof(str) === "string") {
+    if (typeof(str) === 'string') {
         console.log(parseFloat(str.replace(',', '.')) * 100)
     } else {
-        console.log("Wrong argument type" )
+        console.log('Wrong argument type')
     }
 }
 
-console.log("Task 1");
+console.log('Task 1');
 parseAmount('10');
 parseAmount('10,5');
 parseAmount('10,55');
+
+
+
+function count(str, substring) {
+    let counter = 0;
+    let i = 0;
+    str = str.toLowerCase();
+    substring = substring.toLowerCase();
+    while (i != -1) {
+        i = str.indexOf(substring, i);
+        if (i != -1) {
+            counter++;
+            i++;
+        }
+    }
+    return counter
+}
+
+function converter(str) {
+    if (typeof(str) === 'string') {
+        let result = '';
+        for (let i = 0; i < str.length; i++) {
+            if (count(str, str[i]) === 1) {
+                result += '('
+            } else {
+                result += ')'
+            }
+        }
+        console.log(result);
+    } else {
+        console.log('Wrong argument type')
+    }
+}
+
+console.log('Task 2');
+converter('din');
+converter('recede');
+converter('Success');
+converter('(( @');
