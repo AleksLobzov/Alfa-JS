240c8a37b397b032b4a9d198b7295093785d4c36
Alexey Lobzov <aleks.lobzov@gmail.com>
2017-10-17 07:44:00 +0300

    hw2 optimized converter function

--- homework 2/src/converter.js
+++ homework 2/src/converter.js
@@ -5,42 +5,33 @@ export default function converter(operations) {
     // Заполняем массивы операций и ссылок
     operations.forEach(function(item, i, operations) {
 
+        // Инициируем свойства объекта
         if (semiResultObj[item.date] === undefined) {
-
             semiResultObj[item.date] = { "operations": [], "links": [] };
-            semiResultObj[item.date]["operations"].push(item);
-
-            const links = item['links'];
-            links.forEach(function(item2, i2, links) {
-                semiResultObj[item.date]["links"].push(item2);
-            });
-
-        } else {
-
-            semiResultObj[item.date]["operations"].push(item);
-
-            const links = item['links'];
-            links.forEach(function(item2, i2, links) {
-                semiResultObj[item.date]["links"].push(item2);
-            });
-
         };
+
+        // Заполняем массив операций
+        semiResultObj[item.date]["operations"].push(item);
+
+        // Заполняем массив ссылок
+        const links = item['links'];
+        links.forEach(function(item2, i2, links) {
+            semiResultObj[item.date]["links"].push(item2);
+        });
 
     });
 
     // Сортируем свойства объекта по датам в порядке убывания
     const resultObj = {};
 
-    const sortedSemiObj = Object.keys(semiResultObj)
-                                .sort()
-                                .reverse()
-                                .map(
-                                     function(key) {
-
-                                         resultObj[key] = semiResultObj[key]
-
-                                     }
-                                );
+    Object.keys(semiResultObj)
+          .sort()
+          .reverse()
+          .map(
+              function(key) {
+                  resultObj[key] = semiResultObj[key];
+              }
+          );
 
     console.log(resultObj);
 
